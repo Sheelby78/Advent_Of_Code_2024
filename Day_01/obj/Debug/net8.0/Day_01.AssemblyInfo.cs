448f09a9f6923d0028892e1cdea1d96e0710bbdb
Patryk Dulęba <patryczek816@gmail.com>
2024-12-02 20:51:43 +0100

    Day2

--- Day_01/obj/Debug/net8.0/Day_01.AssemblyInfo.cs
+++ Day_01/obj/Debug/net8.0/Day_01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2a6f106aaad1027daea3821047f7edd4cb25cbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+843ad840a51eb252d3129de32c40b7a7b367957a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Day_02/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace Day_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PartOne();
+            PartTwo();
+        }
+
+        static void PartOne()
+        {
+            var numbers = new List<List<int>>();
+            var bad = 0;
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                var row = input.Split(" ").Select(int.Parse).ToList();
+                numbers.Add(row);
+
+                if(!isGood(row))
+                {
+                    bad++;
+                }
+            }
+            Console.WriteLine(numbers.Count() - bad);
+        }
+
+        static void PartTwo()
+        {
+            var numbers = new List<List<int>>();
+            var bad = 0;
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                var row = input.Split(" ").Select(int.Parse).ToList();
+                numbers.Add(row);
+
+
+                if (!isGood(row))
+                {
+                    var anyGood = false;
+                    var temp = new List<List<int>>();
+
+                    for (int i = row.Count(); i != 0; i--)
+                    {
+                        var temp2 = new List<int>();
+                        temp2.AddRange(row);
+                        temp2.RemoveAt(i - 1);
+                        temp.Add(temp2);
+                    }
+
+                    foreach (var x in temp)
+                    {
+                        if (isGood(x))
+                        {
+                            anyGood = true;
+                            break;
+                        }
+                    }
+                    if (!anyGood)
+                    {
+                        bad++;
+                    }
+                }
+            }
+            Console.WriteLine(numbers.Count() - bad);
+        }
+
+        static bool isGood(List<int> row)
+        {
+            if (row.SequenceEqual(row.OrderByDescending(x => x)) || row.SequenceEqual(row.OrderBy(x => x)))
+            {
+                for (int i = 0; i < row.Count() - 1; i++)
+                {
+                    var temp = Math.Abs(row[i] - row[i + 1]);
+
+                    if (temp > 3 || temp == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
