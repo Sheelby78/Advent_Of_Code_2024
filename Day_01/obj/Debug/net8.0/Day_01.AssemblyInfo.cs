843ad840a51eb252d3129de32c40b7a7b367957a
Patryk Dulęba <patryczek816@gmail.com>
2024-12-02 19:13:42 +0100

    fix

--- Day_01/obj/Debug/net8.0/Day_01.AssemblyInfo.cs
+++ Day_01/obj/Debug/net8.0/Day_01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2a6f106aaad1027daea3821047f7edd4cb25cbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
