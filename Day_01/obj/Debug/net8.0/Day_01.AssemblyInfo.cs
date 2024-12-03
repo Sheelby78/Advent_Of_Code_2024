4490f491c7ea1bce7c1facc7d8d83f92979f930d
Patryk Dulęba <patryczek816@gmail.com>
2024-12-03 20:49:29 +0100

    Day 3

--- Day_01/obj/Debug/net8.0/Day_01.AssemblyInfo.cs
+++ Day_01/obj/Debug/net8.0/Day_01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+843ad840a51eb252d3129de32c40b7a7b367957a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+448f09a9f6923d0028892e1cdea1d96e0710bbdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Day_03/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PartOne();
+            PartTwo();
+        }
+
+        static void PartOne()
+        {
+            var result = 0;
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                result += Calculate(input);
+
+            }
+
+            Console.WriteLine(result);
+        }
+
+        static void PartTwo()
+        {
+            var input = "";
+            var result = 0;
+
+            while (true)
+            {
+                var singleInput = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(singleInput))
+                {
+                    break;
+                }
+
+                input += singleInput;
+                
+            }
+
+            var between = @"do\(\)(.*?)don't\(\)";
+
+            var first = Regex.Match(input, @"(.*?)don't\(\)");
+
+            var finded = Regex.Matches(input, between);
+
+            result += Calculate(first);
+
+            foreach (var f in finded)
+            {
+                result += Calculate(f);
+            }
+
+            Console.WriteLine(result);
+        }
+
+        static int Calculate(Object? finded)
+        {
+            var sum = 0;
+            var pattern = @"mul\((\d+),(\d+)\)";
+            var findedCommends = Regex.Matches(finded.ToString(), pattern);
+
+            foreach (var f in findedCommends)
+            {
+                var numbers = Regex.Matches(f.ToString(), @"\d+");
+                sum += (int.Parse(numbers[0].Value) * int.Parse(numbers[1].Value));
+            }
+
+            return sum;
+        }
+    }
+}
